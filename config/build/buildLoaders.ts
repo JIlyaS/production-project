b722449efb8ab26cbe4b408e70780a66b2e58552
Колмаков Илья Григорьевич (4057328) <ikolmakov@vtb.ru>
2022-09-19 00:30:36 +0500

    initial commit

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,64 @@
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  
+  const htmlLoader = {
+    test: /\.html$/,
+    loader: 'html-loader'
+  };
+
+    const cssLoader = {
+      test: /\.s[ac]ss$/i,
+      use: [
+        // // Creates `style` nodes from JS strings
+        // "style-loader",
+        options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+        // Translates CSS into CommonJS
+         {
+            loader: "css-loader",
+            options: {
+              modules: {
+                auto: (resPath: string) => resPath.includes('.module.'),
+                localIdentName: options.isDev ? '[path][name]__[local]' : '[hash:base64:8]',
+              },
+            },
+          },
+        // Compiles Sass to CSS
+        "sass-loader",
+      ],
+     }; 
+
+    // Если не используем TS - нужен babel-loader
+    const typescriptLoader = {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    };
+
+    // const fontLoader = {
+    //   test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+    //   use: [
+    //     {
+    //       loader: 'file-loader',
+    //       options: {
+    //         name: '[name].[ext]',
+    //         outputPath: 'fonts/'
+    //       }
+    //     }
+    //   ],
+    // };
+
+    const fontLoader = {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+    }
+
+  return [
+      htmlLoader,
+      typescriptLoader,
+      cssLoader,
+      fontLoader,
+    ];
+}
